85162b93e92c50856dcf9a87ff2cb8846193c3e0
Kun Yu <kun.yu25@myhunter.cuny.edu>
2020-11-02 15:41:19 -0500

    progress on bulk adding

--- backend/models/index.js
+++ backend/models/index.js
@@ -5,16 +5,17 @@ const dbConfig = require("../config/config.js");
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: dbConfig.dialect,
-    dialectOption: {
-        ssl: true,
-        native: true,
-    },
-    define: {
-        timestamps: true,
-    }
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	dialect: dbConfig.dialect,
+	dialectOption: {
+		ssl: true,
+		native: true,
+	},
+	define: {
+		timestamps: true,
+	},
+	timestamp: "America/New_York",
 });
 
 const db = {};
@@ -22,24 +23,29 @@ const db = {};
 // Hack to initialize Sequalize with all models
 const basename = path.basename(__filename);
 fs.readdirSync(__dirname)
-    .filter((file) => {
-        return (
-            file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-        );
-    })
-    .forEach((file) => {
-        var model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-        db[model.name] = model;
-        console.log(db[model.name]);
-    });
+	.filter((file) => {
+		return (
+			file.indexOf(".") !== 0 &&
+			file !== basename &&
+			file.slice(-3) === ".js"
+		);
+	})
+	.forEach((file) => {
+		var model = require(path.join(__dirname, file))(
+			sequelize,
+			Sequelize.DataTypes
+		);
+		db[model.name] = model;
+		console.log(db[model.name]);
+	});
 
 Object.keys(db).forEach((modelName) => {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
-    }
+	if (db[modelName].associate) {
+		db[modelName].associate(db);
+	}
 });
 
-sequelize.sync()
+sequelize.sync();
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 module.exports = db;
--- backend/routes/userZips.js
+++ backend/routes/userZips.js
@@ -9,5 +9,7 @@ router.route("/:id").get(auth, userZipsController.getElement);
 router.route("/").post(auth, userZipsController.insertElement);
 router.route("/:id").put(auth, userZipsController.editElement);
 router.route("/:id").delete(auth, userZipsController.deleteElement);
+router.route("/user/:userId").get(auth, userZipsController.getAllZipsforUser);
+router.route("/zip/:zipId").get(auth, userZipsController.getAllUsersforZip);
 
 module.exports = router;
