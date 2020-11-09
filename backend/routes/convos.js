0c893ac8e62ab74c02cb7492509902c22472264b
TJ Westfall <thomas.westfall64@myhunter.cuny.edu>
2020-11-09 01:43:06 -0500

    more message routes

--- backend/routes/convos.js
+++ backend/routes/convos.js
@@ -5,6 +5,7 @@ const convoController = require("../controllers/convos");
 //router.route("/").get(postController.getAllPosts);
 router.route("/:userId").post(convoController.createConvo);
 router.route("/:userId").get(convoController.getAllConvosForUser);
-//router.route("/:postId").get(postController.getPostById);
+router.route("/:userId/:convoId").put(convoController.leaveConvo);
+router.route("/:convoId").delete(convoController.deleteConvo);
 
 module.exports = router;
