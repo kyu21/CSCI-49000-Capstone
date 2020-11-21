8e13d7dbddde8dafe8d816e46e977595a3e056bc
kyu21 <kun.yu25@myhunter.cuny.edu>
2020-11-21 17:18:03 -0500

    add landing page for /api endpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res, next) => res.send("LocalHelper Backend API"));
+
 // Subrouters;
 const authRouter = require("./auth");
 const userRouter = require("./users");
