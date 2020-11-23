2ddd3e974dab0a7c62e828748d64e2bc42ed4373
kyu21 <kun.yu25@myhunter.cuny.edu>
2020-11-22 19:53:05 -0500

    remove redundant files/endpoints

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -12,9 +12,6 @@ const postsRouter = require("./posts");
 const messagesRouter = require("./messages");
 const convosRouter = require("./convos");
 
-const postZipsRouter = require("./postZips");
-const postInterestsController = require("./postInterests")
-
 // Mount our subrouters to assemble our apiRouter;
 router.use("/auth", authRouter);
 router.use("/users", userRouter);
@@ -23,9 +20,6 @@ router.use("/posts", postsRouter);
 
 router.use("/messages", messagesRouter);
 router.use("/convos", convosRouter);
-
-router.use("/postZips", postZipsRouter);
-router.use("/postInterests", postInterestsController)
 
 // Error handling middleware;
 router.use((req, res, next) => {
